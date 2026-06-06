1eb2c02072f545490b88d626c083633466b59302
PVALDEZ7 <pv091465@gmail.com>
2026-06-06 10:39:03 -0500

    Agregando archivos CSS y JS

---
+++ assets/js/objetos.js
@@ -0,0 +1,14 @@
+
+
+
+const personas = {
+    nombre: "Pedro",
+    apellido: "Valdez",
+    edad: 25,
+}
+
+console.log(personas.nombre);
+console.log(personas.apellido);
+console.log(personas.edad);
+console.log(personas);
+
